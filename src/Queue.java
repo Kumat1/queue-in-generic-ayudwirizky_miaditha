76928d49708b788a8775d02e7493ac709690250f
HP <rahmamia83@gmail.com>
2017-04-03 20:05:42 +0700

    Nama: Mia Rahma Ditha Nasutioj(151402011)
    Disini, Queue memakai nilai statis yang berarti nilai sudah dimasukkan ke dalam parameter fungsi. Lalu memakai template, karena template bisa meringkas codingan dan juga bisa digunakan pada beberapa tipe data. Disini, saya menggunakan tipe data integer.
    Queue juga memiliki beberapa operator, seperti push untuk memasukkan nilai pada queue pada fungsi enqueue. Pop untuk menghapus satu tipe data pada queue dan digunakan pada fungsi dequeue. lalu ada fungsi display untuk menampilkan data queue. Dan fungsi clear untuk menghapus seluruh data queue.

---
+++ src/Queue.java
@@ -0,0 +1,61 @@
+import java.lang.reflect.Array;
+
+/**
+ * Created by HP on 4/2/2017.
+ */
+public class Queue <T> {
+    public T[] array;
+    public int tail;
+    public Queue(Class<?> classname)
+    {
+        array=(T[]) Array.newInstance(classname, 10);
+        tail=-1;
+    }
+
+    public static void main(String[] args)
+    {
+        Queue<Integer> q = new Queue<Integer>(Integer.class);
+        q.enq(2);
+        q.enq(6);
+        q.enq(4);
+        q.enq(8);
+        q.enq(3);
+        q.display();
+        System.out.println();
+        q.deq();
+        q.deq();
+        q.display();
+        System.out.println();
+        q.clear();
+        q.display();
+    }
+
+    public void enq(T item)
+    {
+        tail++;
+        array[tail]=item;
+    }
+
+    public void deq()
+    {
+        for(int i=0;i<=tail;i++)
+        {
+            array[i]=array[i+1];
+        }
+        tail--;
+    }
+
+    public void display()
+    {
+        for(int i=0;i<=tail;i++)
+        {
+            System.out.print(array[i]);
+            System.out.print(" ");
+        }
+    }
+
+    public void clear()
+    {
+        tail=-1;
+    }
+}
